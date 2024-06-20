e9c0f644dabfa7f09fd513302ec3e8b0c22145b3
Ricardo chies <ricardochies5@gmail.com>
2024-06-20 14:45:08 -0300

    Implementantando rotas artistas e artistas por nome

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -4,10 +4,23 @@ using ScreenSound.Models;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/Artistas", () =>
 {
     var dal = new ScreenSoundDAL<Artista>(new Context());
-    return dal.Listar();
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Artistas/{nome}", (string nome) =>
+{
+    var dal = new ScreenSoundDAL<Artista>(new Context());
+    var artista = dal.RecuperarPor(artista => artista.Nome.ToUpper().Equals(nome.ToUpper()));
+
+    if (artista is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(artista);
 });
 
 app.Run();
