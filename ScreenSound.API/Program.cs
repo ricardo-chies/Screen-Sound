0b2156cf4e341441d688e3489d3a3408a2db3c05
Ricardo chies <ricardochies5@gmail.com>
2024-06-20 15:47:34 -0300

    Implementando documentação Swagger

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using ScreenSound.API.Endpoints;
 using ScreenSound.Data;
 using ScreenSound.Models;
@@ -8,9 +9,29 @@ builder.Services.AddDbContext<Context>();
 builder.Services.AddTransient<ScreenSoundDAL<Artista>>();
 builder.Services.AddTransient<ScreenSoundDAL<Musica>>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen( c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Description = "Esta Aplicação tem como objetivo disponibilizar endpoints referentes a Artistas e Músicas.",
+        Contact = new OpenApiContact
+        {
+            Name = "Ricardo Chies",
+            Email = "chies.dev@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/ricardo-chies-087557216")
+        },
+        Title = "Screen Sound API",
+        Version = "v1"
+    });
+});
+
 var app = builder.Build();
 
 app.AddEnpointsArtistas();
 app.AddEndpointsMusicas();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
