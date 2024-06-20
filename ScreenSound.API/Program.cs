6dbe6d8e516393d6fa97e301b6108e6cee4d6fab
Ricardo chies <ricardochies5@gmail.com>
2024-06-20 15:24:39 -0300

    Implementantando endpoints musicas

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<Context>();
 builder.Services.AddTransient<ScreenSoundDAL<Artista>>();
+builder.Services.AddTransient<ScreenSoundDAL<Musica>>();
 
 var app = builder.Build();
 
@@ -61,5 +62,58 @@ app.MapPut("/Artistas", ([FromServices] ScreenSoundDAL<Artista> dal, [FromBody] 
     dal.Atualizar(artista);
     return Results.Ok();
 });
+
+app.MapGet("/Musicas", ([FromServices] ScreenSoundDAL<Musica> dal) =>
+{
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Musicas/{nome}", ([FromServices] ScreenSoundDAL<Musica> dal, string nome) =>
+{
+    var musica = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(musica);
+
+});
+
+app.MapPost("/Musicas", ([FromServices] ScreenSoundDAL<Musica> dal, [FromBody] Musica musica) =>
+{
+    dal.Adicionar(musica);
+    return Results.Ok();
+});
+
+app.MapDelete("/Musicas/{id}", ([FromServices] ScreenSoundDAL<Musica> dal, int id) => {
+    var musica = dal.RecuperarPor(a => a.Id == id);
+
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+
+    dal.Deletar(musica);
+    return Results.NoContent();
+
+});
+
+app.MapPut("/Musicas", ([FromServices] ScreenSoundDAL<Musica> dal, [FromBody] Musica musicaAtualizada) => {
+    var musica = dal.RecuperarPor(a => a.Id == musicaAtualizada.Id);
+
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+
+    musica.Nome = musicaAtualizada.Nome;
+    musica.AnoLancamento = musicaAtualizada.AnoLancamento;
+
+    dal.Atualizar(musica);
+    return Results.Ok();
+});
+
 
 app.Run();
