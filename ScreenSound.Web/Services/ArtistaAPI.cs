aafac75165ea351ece7c242ea2217d1b1aed2eec
Ricardo chies <ricardochies5@gmail.com>
2024-06-24 18:03:12 -0300

    Ajustes Layout

--- ScreenSound.Web/Services/ArtistaAPI.cs
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -1,4 +1,5 @@
-﻿using ScreenSound.Web.Responses;
+﻿using ScreenSound.Web.Requests;
+using ScreenSound.Web.Responses;
 using System.Net.Http.Json;
 
 namespace ScreenSound.Web.Services;
@@ -10,8 +11,29 @@ public class ArtistaAPI
     {
         _httpClient = factory.CreateClient("API");
     }
+
     public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
     {
         return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
     }
+
+    public async Task AddArtistaAsync(ArtistaRequest artista)
+    {
+        await _httpClient.PostAsJsonAsync("artistas", artista);
+    }
+
+    public async Task DeleteArtistaAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"artistas/{id}");
+    }
+
+    public async Task<ArtistaResponse?> GetArtistaPorNomeAsync(string nome)
+    {
+        return await _httpClient.GetFromJsonAsync<ArtistaResponse>($"artistas/{nome}");
+    }
+    public async Task UpdateArtistaAsync(ArtistaRequestEdit artista)
+    {
+        await _httpClient.PutAsJsonAsync($"artistas", artista);
+    }
+
 }
