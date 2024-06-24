0c9ed36727b28fd73309d06ce8c7d6528a8a3186
Ricardo chies <ricardochies5@gmail.com>
2024-06-24 14:16:31 -0300

    Implementando novas pages

---
+++ ScreenSound.Web/Layout/ScreenSoundPallete.cs
@@ -0,0 +1,16 @@
+﻿using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace ScreenSound.Web.Layout;
+
+public sealed class ScreenSoundPallete : PaletteDark
+{
+    private ScreenSoundPallete()
+    {
+        Primary = new MudColor("#9966FF");
+        Secondary = new MudColor("#F6AD31");
+        Tertiary = new MudColor("#8AE491");
+    }
+
+    public static ScreenSoundPallete CreatePallete => new();
+}
