e5f6f45977b00208c235862c869ee67a0fcaa508
Ricardo chies <ricardochies5@gmail.com>
2024-08-06 20:52:41 -0300

    Implementando teste integração API PostAdicionarArtistaTests

---
+++ ScreenSound.Tests.Integration/API/PostAdicionarArtistaTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+using System.Net;
+using ScreenSound.API.Requests;
+using System.Text;
+
+namespace ScreenSound.Tests.Integration.API
+{
+    public class PostAdicionarArtistaTests
+    {
+        [Fact]
+        public async Task PostAdicionarArtista()
+        {
+            // arrange
+            var app = new ScreenSoundWebApplicationFactory();
+
+            using var client = await app.GetClientWithAccesTokenAsync();
+            var requestUri = "/Artistas";
+
+            var fakePath = Path.Combine(Path.GetTempPath(), "fake", "path");
+
+            Directory.CreateDirectory(Path.Combine(fakePath, "wwwroot", "FotosPerfil"));
+
+            var base64String = Convert.ToBase64String(Encoding.UTF8.GetBytes("fake image data"));
+
+            var artistaRequest = new ArtistaRequest(
+                "Artista de teste",
+                "Criando um artista para teste via teste de integração.",
+                base64String
+            );
+
+            // act
+            var result = await client.PostAsJsonAsync(requestUri, artistaRequest);
+
+            // assert
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
---
+++ ScreenSound.Tests.Integration/ScreenSoundWebApplicationFactory.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using ScreenSound.Data;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace ScreenSound.Tests.Integration
+{
+    public class ScreenSoundWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<Context>));
+                services.AddDbContext<Context>(options => options
+                .UseLazyLoadingProxies()
+                .UseMySql("server = localhost; port = 3307; database = ScreenSound; user = root; password = 123456; Persist Security Info = False", // MySql Container
+                new MySqlServerVersion(new Version(7, 0, 0))));
+
+                base.ConfigureWebHost(builder);
+            });
+        }
+
+        public async Task<HttpClient> GetClientWithAccesTokenAsync()
+        {
+            var client = this.CreateClient();
+            var requestUri = "/auth/login";
+
+            var user = new LoginRequest { Email = "teste@email.com", Password = "Senha@123" };
+            var result = await client.PostAsJsonAsync(requestUri, user);
+
+            var content = await result.Content.ReadAsStringAsync();
+            var action = JsonSerializer.Deserialize<UserTokenDTO>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", action.AccessToken);
+
+            return client;
+        }
+
+    }
+
+    internal class UserTokenDTO
+    {
+        public string? TokenType { get; set; }
+        public string? AccessToken { get; set; }
+        public int ExpiresIn { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
