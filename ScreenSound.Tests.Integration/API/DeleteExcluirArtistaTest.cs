f1b2ebb5f64ca5db0ba9ade6b85787601850c182
Ricardo chies <ricardochies5@gmail.com>
2024-08-06 22:59:46 -0300

    Implementando testes integração API PusAtualizarArtistaTest e DeleteExcluirArtistaTest

---
+++ ScreenSound.Tests.Integration/API/DeleteExcluirArtistaTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace ScreenSound.Tests.Integration.API
+{
+    public class DeleteExcluirArtistaTest(ScreenSoundWebApplicationFactory factory) : IClassFixture<ScreenSoundWebApplicationFactory>
+    {
+        private readonly ScreenSoundWebApplicationFactory _factory = factory;
+
+        [Fact]
+        public async Task DeleteExcluirArtistaNoContent()
+        {
+            // Arrange
+            var artistaExistente = _factory.Context.Artistas
+                .OrderBy(a => a.Id)
+                .LastOrDefault();
+            using var client = await _factory.GetClientWithAccesTokenAsync();
+
+            // Act
+            var result = await client.DeleteAsync("/Artistas/" + artistaExistente.Id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(HttpStatusCode.NoContent, result.StatusCode);
+        }
+    }
+}
---
+++ ScreenSound.Tests.Integration/API/PutAtualizarArtistaTest.cs
@@ -0,0 +1,31 @@
+﻿using ScreenSound.API.Responses;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Tests.Integration.API
+{
+    public class PutAtualizarArtistaTest(ScreenSoundWebApplicationFactory factory) : IClassFixture<ScreenSoundWebApplicationFactory>
+    {
+        private readonly ScreenSoundWebApplicationFactory _factory = factory;
+
+        [Fact]
+        public async Task PutAtualizarArtistaOk()
+        {
+            // Arrange
+            var artistaExistente = _factory.Context.Artistas
+                .OrderBy(a => a.Id)
+                .LastOrDefault();
+            using var client = await _factory.GetClientWithAccesTokenAsync();
+
+            artistaExistente.Nome = "Artista Atualizado";
+            artistaExistente.Bio = "Bio Atualizada";
+
+            // Act
+            var result = await client.PutAsJsonAsync($"/Artistas/", artistaExistente);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}
